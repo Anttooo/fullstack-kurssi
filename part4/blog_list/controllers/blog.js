f358a320daff19b7cbac4e0ca0a99aa202f5b83a
Anttooo <onni.anttoora@gmail.com>
2023-03-20 12:13:08 +0100

    exercises up to 4.12 done

--- part4/blog_list/controllers/blog.js
+++ part4/blog_list/controllers/blog.js
@@ -9,8 +9,12 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
 
-  const savedBlog = await blog.save()
-  response.status(201).json(savedBlog)
+  if (request.body.title && request.body.url) {
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  } else {
+    response.status(400).end()
+  }
 })
 
 module.exports = blogsRouter
--- part4/blog_list/tests/blog_api.test.js
+++ part4/blog_list/tests/blog_api.test.js
@@ -3,15 +3,19 @@ const Blog = require('../models/blog')
 const supertest = require('supertest')
 const helper = require('./test_helper')
 const app = require('../app')
-const note = require('../../part3-notes-backend/models/note')
 const api = supertest(app)
 
 beforeEach(async () => {
   await Blog.deleteMany({})
 
+  // In initialBlogs we have an array of plain Javascript objects
   const blogObjects = helper.initialBlogs
+    // On this line, each of those Javascript objects are transformed into mongoose models
     .map(blog => new Blog(blog))
+  // A mongoose model has the method .save, which is used here to save each model into the db
   const promiseArray = blogObjects.map(blog => blog.save())
+  // By storing the mapping of each blogObject into an array, it's possible to use Promise.all
+  // which means that we're waiting for all of them to be fulfilled before continuing
   await Promise.all(promiseArray)
 })
 
@@ -44,6 +48,52 @@ describe('API', () => {
     expect(titles).toContain(
       'test post'
     )
+  })
+
+  test('Blogs have id as identifier rather than _id', async () => {
+    const blog = await Blog.findOne({title: 'Canonical string reduction'})
+    expect(blog.id).toBeDefined()
+  })
+
+  test('Post without nr. of likes specified defaults likes to 0', async () => {
+    const newBlog = {
+      title: "test post without likes",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/test-post2.html"
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    
+    const blogWithoutLikes = await Blog.findOne({title: 'test post without likes'})
+    expect(blogWithoutLikes.likes).toBe(0)
+  })
+
+  test('Post with url missing is responded to with status 400', async () => {
+    const blogWithoutUrl = {
+      title: "test post without url",
+      author: "Onni"
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(blogWithoutUrl)
+      .expect(400)
+  })
+
+  test('Post with title missing is responded to with status 400', async () => {
+    const blogWithoutTitle = {
+      url: "test post without url",
+      author: "Onni"
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(blogWithoutTitle)
+      .expect(400)
   })
 })
 
